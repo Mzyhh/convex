71859707fad44d0841a0f5a4555fbeb01f50644c
Grigory Sedov <xiiad@mail.ru>
2024-04-08 16:54:33 +0300

    Modification commit

--- convex.py
+++ convex.py
@@ -1,5 +1,36 @@
 from deq import Deq
 from r2point import R2Point
+from math import sqrt
+
+
+class Line:
+    """ Прямая """
+
+    def __init__(self, p: R2Point, q: R2Point):
+        """creating a line using 2 points"""
+
+        self._a = q.y - p.y
+        self._b = p.x - q.x
+        self._c = -(self._a * p.x + self._b * p.y)
+
+    def delta(self, m):
+        """delta is a distance from a point to a line\n
+        delta = (a*x + b*y + c) * sgn(c) / sqrt(a**2 + b**2)\n
+        a, b, c - coefficients in the equation of a line"""
+
+        sgn_c = 1 if self._c >= 0 else -1
+        return (self._a * m.x + self._b * m.y + self._c) * (-sgn_c) /\
+            sqrt(self._a**2 + self._b**2)
+
+    def is_close_p(self, p, dist=1):
+        """the distance between a point (p) and a line is less than dist"""
+
+        return abs(self.delta(p)) < dist
+
+    def is_close_seg(self, p, q, dist=1):
+        """the distance between a segment(p, q) and a line is less than dist"""
+
+        return self.is_close_p(p) and self.is_close_p(q)
 
 
 class Figure:
@@ -10,6 +41,12 @@ class Figure:
 
     def area(self):
         return 0.0
+
+    def close_edges(self):
+        return 0
+
+    def bind_line(self, p, q):
+        self.fl = Line(p, q)
 
 
 class Void(Figure):
@@ -23,6 +60,7 @@ class Point(Figure):
     """ "Одноугольник" """
 
     def __init__(self, p):
+        super().__init__()
         self.p = p
 
     def add(self, q):
@@ -33,26 +71,31 @@ class Segment(Figure):
     """ "Двуугольник" """
 
     def __init__(self, p, q):
+        super().__init__()
         self.p, self.q = p, q
 
     def perimeter(self):
         return 2.0 * self.p.dist(self.q)
 
+    def close_edges(self):
+        return 2 if self.fl.is_close_seg(self.p, self.q) else 0
+
     def add(self, r):
         if R2Point.is_triangle(self.p, self.q, r):
             return Polygon(self.p, self.q, r)
-        elif r.is_inside(self.p, self.q):
-            return self
+        elif self.q.is_inside(self.p, r):
+            return Segment(self.p, r)
         elif self.p.is_inside(r, self.q):
             return Segment(r, self.q)
         else:
-            return Segment(self.p, r)
+            return self
 
 
 class Polygon(Figure):
     """ Многоугольник """
 
-    def __init__(self, a, b, c):
+    def __init__(self, a, b, c, p=None, q=None):
+        super().__init__()
         self.points = Deq()
         self.points.push_first(b)
         if b.is_light(a, c):
@@ -63,12 +106,23 @@ class Polygon(Figure):
             self.points.push_first(c)
         self._perimeter = a.dist(b) + b.dist(c) + c.dist(a)
         self._area = abs(R2Point.area(a, b, c))
+        if q is None:
+            self._close_edges = None
+        else:
+            self.bind_line(p, q)
+            close_points = self.fl.is_close_p(a) + self.fl.is_close_p(b)\
+                + self.fl.is_close_p(c)
+            self._close_edges = 0 if close_points < 2 else\
+                1 if close_points == 2 else 3
 
     def perimeter(self):
         return self._perimeter
 
     def area(self):
         return self._area
+
+    def close_edges(self):
+        return self._close_edges
 
     # добавление новой точки
     def add(self, t):
@@ -87,12 +141,18 @@ class Polygon(Figure):
             self._area += abs(R2Point.area(t,
                                            self.points.last(),
                                            self.points.first()))
+            if self._close_edges is not None:
+                self._close_edges -= self.fl.is_close_seg(self.points.first(),
+                                                          self.points.last())
 
             # удаление освещённых рёбер из начала дека
             p = self.points.pop_first()
             while t.is_light(p, self.points.first()):
                 self._perimeter -= p.dist(self.points.first())
                 self._area += abs(R2Point.area(t, p, self.points.first()))
+                if self._close_edges is not None:
+                    self._close_edges -= self.fl.\
+                        is_close_seg(self.points.first(), p)
                 p = self.points.pop_first()
             self.points.push_first(p)
 
@@ -101,12 +161,19 @@ class Polygon(Figure):
             while t.is_light(self.points.last(), p):
                 self._perimeter -= p.dist(self.points.last())
                 self._area += abs(R2Point.area(t, p, self.points.last()))
+                if self._close_edges is not None:
+                    self._close_edges -= self.fl.\
+                        is_close_seg(self.points.last(), p)
                 p = self.points.pop_last()
             self.points.push_last(p)
 
             # добавление двух новых рёбер
             self._perimeter += t.dist(self.points.first()) + \
                 t.dist(self.points.last())
+            if self._close_edges is not None:
+                self._close_edges += self.fl.\
+                    is_close_seg(t, self.points.first()) + self.fl.\
+                    is_close_seg(t, self.points.last())
             self.points.push_first(t)
 
         return self
@@ -121,3 +188,4 @@ if __name__ == "__main__":
     print(type(f), f.__dict__)
     f = f.add(R2Point(0.0, 1.0))
     print(type(f), f.__dict__)
+    f.bind_line(R2Point(2, 2), R2Point(3, 4))
--- tests/test_convex.py
+++ tests/test_convex.py
@@ -1,7 +1,75 @@
 from pytest import approx
 from math import sqrt
 from r2point import R2Point
-from convex import Figure, Void, Point, Segment, Polygon
+from convex import Figure, Void, Point, Segment, Polygon, Line
+
+
+class TestLine:
+
+    # Вычисление коэффициентов прямой, заданной двумя различными точками
+    #   Прямая-нормис
+    def test_bind1(self):
+        a = Line(R2Point(-2, 3), R2Point(3, 1))
+        assert a._a == -2 and a._b == -5 and a._c == 11
+
+    #   Вертикальная
+    def test_bind2(self):
+        a = Line(R2Point(-5.0, 4.0), R2Point(-5.0, 0.0))
+        assert a._a == -4.0 and a._b == 0.0 and a._c == -20.0
+
+    #   Горизонтальная
+    def test_bind3(self):
+        a = Line(R2Point(1, 3), R2Point(10, 3))
+        assert a._a == 0 and a._b == -9 and a._c == 27
+
+    # Вычисление отклонения
+    def test_delta1(self):
+        a = Line(R2Point(-2, 3), R2Point(3, 1))
+        assert a.delta(R2Point(0.0, 2.2)) == approx(0.0)
+
+    def test_delta2(self):
+        a = Line(R2Point(-5.0, 4.0), R2Point(-5.0, 0.0))
+        assert a.delta(R2Point(0.0, 0.0)) == approx(-5.0)
+
+    def test_delta3(self):
+        a = Line(R2Point(1, 3), R2Point(10, 3))
+        assert a.delta(R2Point(0.0, 4.5)) == approx(1.5)
+
+
+class TestFigure:
+
+    def setup_method(self):
+        self.f = Figure()
+
+    # Вычисление коэффициентов прямой, заданной двумя различными точками
+    #   Прямая-нормис
+    def test_bind1(self):
+        self.f.bind_line(R2Point(-2, 3), R2Point(3, 1))
+        assert self.f.fl._a == -2 and self.f.fl._b == -5 and self.f.fl._c == 11
+
+    #   Вертикальная
+    def test_bind2(self):
+        self.f.bind_line(R2Point(-5.0, 4.0), R2Point(-5.0, 0.0))
+        assert self.f.fl._a == -4.0 and self.f.fl._b == 0.0 and\
+            self.f.fl._c == -20.0
+
+    #   Горизонтальная
+    def test_bind3(self):
+        self.f.bind_line(R2Point(1, 3), R2Point(10, 3))
+        assert self.f.fl._a == 0 and self.f.fl._b == -9 and self.f.fl._c == 27
+
+    # Вычисление отклонения
+    def test_delta1(self):
+        self.f.bind_line(R2Point(-2, 3), R2Point(3, 1))
+        assert self.f.fl.delta(R2Point(0.0, 2.2)) == approx(0.0)
+
+    def test_delta2(self):
+        self.f.bind_line(R2Point(-5.0, 4.0), R2Point(-5.0, 0.0))
+        assert self.f.fl.delta(R2Point(0.0, 0.0)) == approx(-5.0)
+
+    def test_delta3(self):
+        self.f.bind_line(R2Point(1, 3), R2Point(10, 3))
+        assert self.f.fl.delta(R2Point(0.0, 4.5)) == approx(1.5)
 
 
 class TestVoid:
@@ -29,6 +97,10 @@ class TestVoid:
     # При добавлении точки нульугольник превращается в одноугольник
     def test_add(self):
         assert isinstance(self.f.add(R2Point(0.0, 0.0)), Point)
+
+    # Количество особых ребёр
+    def test_edge(self):
+        assert self.f.close_edges() == 0
 
 
 class TestPoint:
@@ -61,6 +133,10 @@ class TestPoint:
     def test_add2(self):
         assert isinstance(self.f.add(R2Point(1.0, 0.0)), Segment)
 
+    # Количество особых ребёр
+    def test_edge(self):
+        assert self.f.close_edges() == 0
+
 
 class TestSegment:
 
@@ -88,11 +164,6 @@ class TestSegment:
     def test_add1(self):
         assert self.f.add(R2Point(0.5, 0.0)) is self.f
 
-    # Он не изменяется в том случае, когда добавляемая точка совпадает
-    # с одним из концов отрезка
-    def test_add1(self):
-        assert self.f.add(R2Point(0.0, 0.0)) is self.f
-
     # При добавлении точки правее двуугольник может превратиться в другой
     # двуугольник
     def test_add2(self):
@@ -107,6 +178,31 @@ class TestSegment:
     def test_add4(self):
         assert isinstance(self.f.add(R2Point(0.0, 1.0)), Polygon)
 
+    # Проверка на принадлежность 1-окрестности открытой прямой
+    def test_edge1(self):
+        self.f.bind_line(R2Point(0.0, 0.5), R2Point(1.0, 0.5))
+        assert self.f.close_edges() == 2
+
+    def test_edge2(self):
+        self.f.bind_line(R2Point(0.5, 0.0), R2Point(0.5, 5.0))
+        assert self.f.close_edges() == 2
+
+    def test_edge3(self):
+        self.f.bind_line(R2Point(0.0, 0.0), R2Point(0.0, 5.0))
+        assert self.f.close_edges() == 0
+
+    def test_edge4(self):
+        self.f.bind_line(R2Point(0.0, 1.0), R2Point(3.0, 1.0))
+        assert self.f.close_edges() == 0
+
+    def test_edge5(self):
+        self.f.bind_line(R2Point(0.0, 1.1), R2Point(3.0, 1.0))
+        assert self.f.close_edges() == 0
+
+    def test_edge6(self):
+        self.f.bind_line(R2Point(0.0, 0.9), R2Point(3.0, 1.0))
+        assert self.f.close_edges() == 2
+
 
 class TestPolygon:
 
@@ -116,6 +212,12 @@ class TestPolygon:
         self.b = R2Point(1.0, 0.0)
         self.c = R2Point(0.0, 1.0)
         self.f = Polygon(self.a, self.b, self.c)
+
+        self.f2 = Polygon(self.a, self.b, self.c, R2Point(0.0, -0.1),
+                          R2Point(1.0, -0.1))
+
+        self.f3 = Polygon(self.a, self.b, self.c, R2Point(0.0, 0.3),
+                          R2Point(.5, .5))
 
     # Многоугольник является фигурой
     def test_figure(self):
@@ -177,3 +279,76 @@ class TestPolygon:
     #   добавление точки может увеличить площадь
     def test_area2(self):
         assert self.f.add(R2Point(1.0, 1.0)).area() == approx(1.0)
+
+    # Тесты на функцию, вычисляющую кол-во ребер, лежащих в 1-окрестность
+    # заданной прямой
+    #   При инициализации
+    def test_edge_init1(self):
+        assert self.f.close_edges() is None
+
+    def test_edge_init2(self):
+        ff = Polygon(self.a, self.b, self.c, R2Point(0.0, 0.5),
+                     R2Point(0.5, 0.0))
+        assert ff.close_edges() == 3
+
+    def test_edge_init2(self):
+        ff = Polygon(self.a, self.b, self.c, R2Point(0.0, -0.1),
+                     R2Point(1.0, -0.1))
+        assert ff.close_edges() == 1
+
+    def test_edge_init3(self):
+        ff = Polygon(self.a, self.b, self.c, R2Point(0.0, -1.1),
+                     R2Point(1.0, -2))
+        assert ff.close_edges() == 0
+
+    #   При добавлении новых точек
+    def test_edge_add1(self):
+        self.f2 = self.f2.add(R2Point(1.0, 1.0))
+        assert self.f2.close_edges() == 1
+
+    def test_edge_add2(self):
+        self.f2 = self.f2.add(R2Point(3.0, 0.5))
+        assert self.f2.close_edges() == 2
+
+    def test_edge_add3(self):
+        self.f2 = self.f2.add(R2Point(.5, -0.5))
+        assert self.f2.close_edges() == 2
+
+    def test_edge_add4(self):
+        self.f2 = self.f2.add(R2Point(.5, -5.0))
+        assert self.f2.close_edges() == 0
+
+    def test_edge_add5(self):
+        self.f2 = self.f2.add(R2Point(.5, -0.5))
+        self.f2 = self.f2.add(R2Point(.8, -0.3))
+        assert self.f2.close_edges() == 3
+
+    def test_edge_add6(self):
+        self.f3 = self.f3.add(R2Point(1.1, 0.4))
+        assert self.f3.close_edges() == 4
+
+    def test_edge_add7(self):
+        self.f3 = self.f3.add(R2Point(1.1, 0.4))
+        self.f3 = self.f3.add(R2Point(0.5, 0.5))
+        self.f3 = self.f3.add(R2Point(-0.5, 0.5))
+        assert self.f3.close_edges() == 5
+
+    def test_edge_add8(self):
+        self.f3 = self.f3.add(R2Point(-2, 0.5))
+        self.f3 = self.f3.add(R2Point(0.6, 0.8))
+        self.f3 = self.f3.add(R2Point(3.0, 4.5))
+        assert self.f3.close_edges() == 2
+
+    def test_edge_add9(self):
+        f4 = Polygon(R2Point(-1.0, 0.0), R2Point(1.0, 0.0), R2Point(0.0, -0.2),
+                     R2Point(-1.0, 0.0), R2Point(1.0, 0.0))
+        f4 = f4.add(R2Point(-0.9, 0.2))
+        f4 = f4.add(R2Point(0.9, 0.2))
+        f4 = f4.add(R2Point(-0.8, 0.3))
+        f4 = f4.add(R2Point(0.8, 0.3))
+        f4 = f4.add(R2Point(-0.6, 0.4))
+        f4 = f4.add(R2Point(0.6, 0.4))
+        f4 = f4.add(R2Point(-0.3, 0.5))
+        f4 = f4.add(R2Point(0.3, 0.5))
+        f4 = f4.add(R2Point(0.0, 3.0))
+        assert f4.close_edges() == 2
